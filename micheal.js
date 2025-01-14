2b76765d5de451d442303ec67ce0a61cc2180c76
Ehapa Micheal <ehapamicheal@gmail.com>
2025-01-14 23:48:48 +0100

    updated portfolio

--- micheal.js
+++ micheal.js
@@ -50,7 +50,6 @@ window.onscroll = () => {
 
 
 //toggle menu
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -69,121 +68,6 @@ if (yearSpan) {
   yearSpan.textContent = currentYear;
 }
 
-
-// Initialize EmailJS with your user ID
-// emailjs.init("ehapamicheal@gmail.com");
-
-// Function to send email
-// function sendEmail() {
-//     // Get form data
-//     var name = document.getElementById('name').value;
-//     var email = document.getElementById('email').value;
-//     var mobileNumber = document.getElementById('mobileNumber').value;
-//     var subject = document.getElementById('subject').value;
-//     var message = document.getElementById('message').value;
-
-//     // Send email
-//     emailjs.send('service_xzdpcmq','template_keilg2d','#contactForm','uqqMBYrxyccHfQz85' {
-//         name: name,
-//         email: email,
-//         mobileNumber: mobileNumber,
-//         subject: subject,
-//         message: message
-//     })
-//     .then(function(response) {
-//         // Email sent successfully
-//         alert('Your message has been sent.');
-//         // You can redirect the user to another page if needed
-//     }, function(error) {
-//         // Error occurred while sending email
-//         console.error('Error sending email:', error);
-//         alert('There was an error sending your message. Please try again later.');
-//     });
-// }
-
-
-// emailjs.init("uqqMBYrxyccHfQz85");
-
-
-// function sendEmail() {
-//     // Get form data
-//     var name = document.getElementById('name').value;
-//     var email = document.getElementById('email').value;
-//     var mobileNumber = document.getElementById('number').value; // Corrected ID
-//     var subject = document.getElementById('subject').value;
-//     var message = document.getElementById('message').value;
-
-//     // Send email
-//     emailjs.send('service_xzdpcmq', 'template_ogsddxl', {
-//         name: name,
-//         email: email,
-//         mobileNumber: mobileNumber,
-//         subject: subject,
-//         message: message
-//     })
-//     .then(function(response) {
-//         // Email sent successfully
-//         alert('Your message has been sent.');
-//         // You can redirect the user to another page if needed
-//     }, function(error) {
-//         // Error occurred while sending email
-//         console.error('Error sending email:', error);
-//         alert('There was an error sending your message. Please try again later.');
-//     });
-// }
-
-
-// Send Email     template_ogsddxl   use this later template_keilg2d
-
-
-
-
-// const contactForm = document.getElementById("contactForm");
-
-// const sendEmail = (e) => {
-//     e.preventDefault()
-
-
-//     emailjs.sendForm('service_xzdpcmq','template_keilg2d','#contactForm','uqqMBYrxyccHfQz85')
-
-//     .then(function(response) {
-//         // Email sent successfully
-//         alert('Your message has been sent.');
-//         // You can redirect the user to another page if needed
-//         }, function(error) {
-//         // Error occurred while sending email
-//         console.error('Error sending email:', error);
-//         alert('There was an error sending your message. Please try again later.');
-//     });
-    
-// }
-
-
-// contactForm.addEventListener('submit', sendEmail)
-
-
-// function sendMessage() {
-//     (function () {
-//         emailjs.init("uqqMBYrxyccHfQz85");
-//     })();
-
-//     let serviceID = "service_xzdpcmq";
-//     let templateID = "template_ogsddxl";
-
-//     let params = {
-//         sendername: document.querySelector("#name").value,
-//         senderemail: document.querySelector("#email").value,
-//         subject: document.querySelector("#subject").value,
-//         nuumber: document.querySelector("#number").value,
-//         message: document.querySelector("#message").value
-//     };
-
-//     emailjs.send(serviceID, templateID, params)
-//     .then(res => {
-//         alert('Thank you, ' + params['sendername'] + '! Your message has been sent.');
-//     })
-//     .catch();
-// }
 
 
 
